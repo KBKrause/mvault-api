a65706b815aa9b4832356ca68d366dbee7611312
Kevin Krause <kkrause@appirio.com>
2019-06-03 17:01:17 -0400

    Sample web request code

---
+++ mvault-api-app/mvault-api-app/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace mvault_api_app
+{
+    public class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+            WebRequest request = WebRequest.Create("https://postman-echo.com/get?foo1=bar1&foo2=bar2");
+            request.Credentials = CredentialCache.DefaultCredentials;
+            WebResponse response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusCode);
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.  
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.  
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.  
+                Console.WriteLine(responseFromServer);
+            }
+
+            // Close the response.  
+            response.Close();
+        }
+    }
+}
