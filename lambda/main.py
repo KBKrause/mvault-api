78f56d5d8f7bc7211a851d55c423a12064ba52ba
Kevin Krause <kkrause@appirio.com>
2019-06-05 12:09:31 -0400

    Access resources in S3

--- lambda/main.py
+++ lambda/main.py
@@ -2,7 +2,22 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-    s3_client = boto3.client('s3')
+    print('Hello!')
+
+    s3 = boto3.client('s3')
+    bucket = 'mvault-api'
+    key = 'cloud-kicks.png'
+
+    try:
+        data = s3.get_object(Bucket=bucket, Key=key)
+        json_data = data['Body'].read()
+
+        return json_data
+
+    except Exception as e:
+        print(e)
+        raise e
+
     # TODO implement
     return {
         'statusCode': 200,
